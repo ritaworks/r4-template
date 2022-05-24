a1000ffe516baeca15b6c93bf2694b1f9a0a3467
izuharaaa <izuhara@ritaworks.jp>
2022-05-24 19:10:56 +0900

    formJSに説明追加、その他調整

--- data/themes/THEME-NAME/js/module/form.js
+++ data/themes/THEME-NAME/js/module/form.js
@@ -1,75 +1,99 @@
-////////////
-// Settings
-// オプションはそれぞれ設定する
-////////////
-const ADDRESS = false;
-const ADDRESS_NAME = 'address'; //form_xxxxxx
-const TEL = []; //form_xxxxxx  ex) ['inq01', 'inq02']
-const SUM = false;
-const DATEPICKER = false;
+/*
 
-////////////
-// 合計
-////////////
-if (SUM) {
-  $('.inputForm').sum({
-    type: ['radio', 'checkbox', 'text', 'select'],
-    pre_split: '｜',
-    post_split: '円',
-    sum_field: 'form_sum', //合計金額表示欄（自由文「form_inqXX」）
-    sum_class: '',
-    add_price: [], //3桁区切りなし
-    add_field: [],
-    remove_field: [],
-  });
-}
+■ フォーム機能を使う前に確認して下さい
+  1. 住所自動入力
+    - form.js
+    - form/jquery.jpostal.js
+  2. 電話番号調整
+    - form.js
+    - form/libphonenumber-js.min.js
+  3. 日付選択
+    - デフォルト機能
+        OR
+    - ../../css/module/form/jquery-ui.min.css（色を変更したい場合は、違うcssを読み込む）
+    - form/jquery-ui.min.js
+    - form/jquery.ui.datepicker-ja.min.js
+  4. 合計金額計算
+    - form/jquery.sum.js
 
-////////////
-// 郵便番号
-// 入力欄が複数ある場合は追加設定必要
-////////////
-if (ADDRESS && ADDRESS_NAME) {
-  let address_zipcode = 'input[name="form_' + ADDRESS_NAME + '_zipcode"]';
-  let address_address = 'input[name="form_' + ADDRESS_NAME + '_address"]';
-  $(address_zipcode).jpostal({
-    postcode: [address_zipcode],
-    address: {
-      address_address: '%3%4%5',
-    }
-  });
-  $(address_zipcode).change(function () {
-    let zipcode = $(this).val();
-    if (zipcode.slice(3, 4) != "-" && zipcode.length > 2) {
-      zipcode = zipcode.slice(0, 3) + "-" + zipcode.slice(3);
-      $(this).val(zipcode);
-    }
-  });
-}
+■ 絞り込み機能
+  1. form/jquery.resultStay.js
 
-////////////
-// 電話番号
-////////////
-if (TEL.length > 0) {
-  let tel_name = '';
-  // バリデーション関数
-  var validateTelNeo = function (value) {
-    return /^[0０]/.test(value) && libphonenumber.isValidNumber(value, 'JP');
-  }
-  // 整形関数
-  var formatTel = function (value) {
-    return new libphonenumber.AsYouType('JP').input(value);
-  }
-  var main = function (tel) {
-    if (!validateTelNeo(tel)) {
-      return tel;
+※※※ 具体的な使用方法についてはwiki等を確認して下さい ※※※
+
+*/
+
+
+(function ($) {
+  $.fn.form = function (params) {
+
+    ////////////
+    // Settings
+    // オプションはそれぞれ設定する
+    ////////////
+    var defs = {
+      //form_xxxxxx  ex) ['inq01', 'inq02']
+      address: [],
+      tel: [],
+    };
+    // データ上書き
+    var config = $.extend({}, defs, params);
+    var form = this;
+
+    ////////////
+    // 郵便番号
+    // 入力欄が複数ある場合は追加設定必要
+    ////////////
+    if (config.address.length > 0) {
+      let address_zipcode = '';
+      let address_address = '';
+      let zipcode = '';
+      for (let i = 0; config.address.length > i; i++) {
+        address_zipcode = 'input[name="form_' + config.address[i] + '_zipcode"]';
+        address_address = 'input[name="form_' + config.address[i] + '_address"]';
+        form.find(address_zipcode).jpostal({
+          postcode: [address_zipcode],
+          address: {
+            [address_address]: '%3%4%5',
+          }
+        });
+        form.find(address_zipcode).change(function () {
+          zipcode = $(this).val();
+          if (zipcode.slice(3, 4) != "-" && zipcode.length > 2) {
+            zipcode = zipcode.slice(0, 3) + "-" + zipcode.slice(3);
+            $(this).val(zipcode);
+          }
+        });
+      }
     }
-    var formattedTel = formatTel(tel)
-    return formattedTel;
-  }
-  for (let i = 0; TEL.length < i; i++) {
-    tel_name = '[name="form_' + TEL[i] + '"]';
-    $(tel_name).blur(function () {
-      $(this).val(main($(this).val()));
-    });
-  }
-}
+
+    ////////////
+    // 電話番号
+    ////////////
+    if (config.tel.length > 0) {
+      let tel_name = '';
+      // バリデーション関数
+      var validateTelNeo = function (value) {
+        return /^[0０]/.test(value) && libphonenumber.isValidNumber(value, 'JP');
+      }
+      // 整形関数
+      var formatTel = function (value) {
+        return new libphonenumber.AsYouType('JP').input(value);
+      }
+      var main = function (tel) {
+        if (!validateTelNeo(tel)) {
+          return tel;
+        }
+        var formattedTel = formatTel(tel)
+        return formattedTel;
+      }
+      for (let i = 0; config.tel.length > i; i++) {
+        tel_name = '[name="form_' + config.tel[i] + '"]';
+        form.find(tel_name).blur(function () {
+          $(this).val(main($(this).val()));
+        });
+      }
+    }
+
+  };
+})(jQuery);
