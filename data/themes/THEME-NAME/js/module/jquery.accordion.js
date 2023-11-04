93f04bc8320746d37543c4717eeb30eea1c6211e
beomsu6567 <beomsu6567@gmail.com>
2023-11-04 17:36:03 +0900

    jquery.accordion最新更新
    23.11.04 アコーディオン調整

--- data/themes/THEME-NAME/js/module/jquery.accordion.js
+++ data/themes/THEME-NAME/js/module/jquery.accordion.js
@@ -14,23 +14,45 @@
     var s = {
       size: 'pc', // accordionが動くサイズを設定します。（tbで設定する場合タブレット以下のみ動く）
       SP_WIDTH: 768, // タブレット時に動かす横幅のサイズを設定します。
+      otherClose: false,
       aTagAutoClose: false, // accordionで開いたボックスの中にあるaタグをクリックした場合に自動的に開いたボックスをそのままにするか閉じるかする（otherCloseとは違います）
       textChange: false, // accordionで開いたボックスの中にあるaタグをクリックした時にaccordionのテキストをaタグと同じテキストにするかどうか
       aTagClassAdd: false, // accordionで開いたボックスの中にあるaタグをクリックした場合、aタグのclassをaccodionのタグにもclassを追加する（親要素のタブによって色などが変わる場合、使います。、addClassとは違います。）
       position: 'normal', // accordionボックスのpositionを設定できます。
-      borderRadius: 0 //accordionのテキストにborder-radiusが設定されている場合その数値と同じくする
+      borderRadius: 0, //accordionのテキストにborder-radiusが設定されている場合その数値と同じくする
+      pcHide: false, // pc時にボタンを無くす
+      parentAccordion: false, // 直下の要素ではなく親の要素
+      btnHeight: true, // parentAccordionがtrueの場合動きます、buttonの高さを入れるか入れないか
+      parentHeight_PC: 100, // parentAccordionがtrueの場合動きます、親要素高さの指定
+      parentHeight_SP: 100,
     }
 
     // 横幅のresize時にも反応できるようにするためのもの
     var oneChange = [0, 1];
+    var targetTag = $(this);
 
     s = $.extend({}, s, option);
 
     // accordionの動きを動きを入れてくれるイベント（slideToggle）
     function accordionEvent(e) {
-      e.toggleClass('active');
-      e.next().slideToggle();
-      e.next().addClass('check');
+      if (e.hasClass('active')) {
+        e.removeClass('active');
+        e.next().slideToggle();
+        e.next().addClass('check');
+      } else {
+        if (s['otherClose']) {
+          $(targetTag).each(function (i, t) {
+            if ($(t).hasClass('active')) {
+              $(t).removeClass('active');
+              $(t).next().slideToggle();
+            }
+          })
+        }
+
+        e.addClass('active');
+        e.next().slideToggle();
+        e.next().addClass('check');
+      }
     }
 
     // accordionボタンにマウスポイントのCSSを追加
@@ -42,36 +64,94 @@
     // accordionで開かれるボックスのposition設定
     function positionEvent(e) {
       e.parent().css('position', 'relative');
-      e.parent().css('z-index', '10');
+      e.parent().css('z-index', '2');
       e.next().css('position', 'absolute');
       e.next().css('top', e.outerHeight() - 1 - s['borderRadius'] + 'px');
     }
 
-    // pcかtbかを判別してマウスポイントのcssを出すかどうかの判別
-    if (s['size'] == 'pc') {
-      pointEvent($(this));
-      if (s['position'] == 'absolute') {
-        positionEvent($(this));
+    function parentAccordionEvent(e) {
+      if ($(e).hasClass('active')) {
+        $(e).removeClass('active');
+        $(e).parent().removeClass('active');
+        $(e).parent().height(s['parentHeight_PC']);
+      } else {
+        var heightNum = 0;
+        $(e).siblings().each(function (i, t) {
+          heightNum += $(t).outerHeight();
+        })
+        if (s['btnHeight']) {
+          heightNum += $(e).outerHeight() + 20;
+        }
+        $(e).parent().height(heightNum);
+        $(e).addClass('active');
+        $(e).parent().addClass('active');
       }
-    } else if (s['size'] == 'tb') {
-      if ($(window).width() <= s['SP_WIDTH']) {
-        pointEvent($(this));
-        oneChange[0]++;
-        oneChange[1]--;
+    }
 
+    if (s['parentAccordion']) {
+      if (s['size'] == 'pc') {
+        $(this).parent().css('overflow', 'hidden');
+        $(this).parent().css('position', 'relative');
+        $(this).css('cursor', 'pointer');
+        $(this).parent().css('transition', '0.3s');
+        if ($(window).width() <= s['SP_WIDTH']) {
+          $(this).parent().height(s['parentHeight_SP']);
+        } else {
+          $(this).parent().height(s['parentHeight_PC']);
+        }
+      } else if (s['size'] == 'tb') {
+        if ($(window).width() <= s['SP_WIDTH']) {
+          oneChange[0]++;
+          oneChange[1]--;
+          $(this).parent().height(s['parentHeight_SP']);
+          $(this).parent().css('overflow', 'hidden');
+          $(this).css('cursor', 'pointer');
+          $(this).parent().css('transition', '0.3s');
+        } else {
+          $(this).css('display', 'none');
+          $(this).parent().css('height', '');
+          $(this).parent().css('overflow', '');
+          $(this).css('cursor', '');
+          $(this).parent().css('transition', '');
+        }
+      }
+    } else {
+      // pcかtbかを判別してマウスポイントのcssを出すかどうかの判別
+      if (s['size'] == 'pc') {
+        pointEvent($(this));
         if (s['position'] == 'absolute') {
           positionEvent($(this));
+        }
+      } else if (s['size'] == 'tb') {
+        if ($(window).width() <= s['SP_WIDTH']) {
+          pointEvent($(this));
+          oneChange[0]++;
+          oneChange[1]--;
+
+          if (s['position'] == 'absolute') {
+            positionEvent($(this));
+          }
         }
       }
     }
 
-    // accordionを動かすかどうか（pc、tb）
     $(this).on('click', function () {
-      if (s['size'] == 'pc') {
-        accordionEvent($(this));
-      } else if (s['size'] == 'tb') {
-        if ($(window).width() <= s['SP_WIDTH']) {
+      if (s['parentAccordion']) {
+        if (s['size'] == 'pc') {
+          parentAccordionEvent($(this));
+        } else if (s['size'] == 'tb') {
+          if ($(window).width() <= s['SP_WIDTH']) {
+            parentAccordionEvent($(this));
+          }
+        }
+      } else {
+        // accordionを動かすかどうか（pc、tb）
+        if (s['size'] == 'pc') {
           accordionEvent($(this));
+        } else if (s['size'] == 'tb') {
+          if ($(window).width() <= s['SP_WIDTH']) {
+            accordionEvent($(this));
+          }
         }
       }
     });
@@ -84,10 +164,8 @@
           $(this).parents('.check').prev().text($(this).text());
         }
         if (s['aTagClassAdd']) {
-          console.log(classChange);
           if (classChange) {
             $(this).parents('.check').prev().removeClass(classChange);
-            console.log(classChange);
           }
           classChange = this.classList[0];
           $(this).parents('.check').prev().addClass(this.classList[0]);
@@ -118,35 +196,16 @@
       }
     });
 
-
-    // 画面のresizeにも対応できる
-    var tag = $(this);
-    $(window).resize(function () {
-      if (s['size'] == 'pc') {
-        pointEvent(tag);
-      } else if (s['size'] == 'tb') {
-        if ($(window).width() <= s['SP_WIDTH']) {
-          if (oneChange[0] == 0) {
-            pointEvent(tag);
-            oneChange[0]++;
-            oneChange[1]--;
-
-            if (s['position'] == 'absolute') {
-              positionEvent(tag);
-            }
-          }
+    function pcHideEvent(e) {
+      if (s['pcHide']) {
+        if ($(window).width() > s['SP_WIDTH']) {
+          $(e).hide();
         } else {
-          if (oneChange[1] == 0) {
-            tag.css('cursor', '');
-            tag.next().css('display', '');
-            tag.parent().css('position', '');
-            tag.next().css('position', '');
-            tag.next().css('top', '');
-            oneChange[0]--;
-            oneChange[1]++;
-          }
+          $(e).show();
         }
       }
-    })
+    }
+
+    pcHideEvent($(this));
   }
 })(jQuery);
